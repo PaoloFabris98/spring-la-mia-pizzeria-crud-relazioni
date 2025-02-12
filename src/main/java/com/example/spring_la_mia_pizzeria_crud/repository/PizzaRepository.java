0baf66eae984d6fff07980a912298a725e9cc345
PaoloFabris98 <f98paolo@gmail.com>
2025-02-12 21:32:12 +0100

    feat: better

--- src/main/java/com/example/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
@@ -3,6 +3,8 @@ package com.example.spring_la_mia_pizzeria_crud.repository;
 import java.util.ArrayList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.spring_la_mia_pizzeria_crud.java.database_controller.*;
@@ -10,4 +12,9 @@ import com.example.spring_la_mia_pizzeria_crud.java.database_controller.*;
 @Repository
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
     ArrayList<Pizza> findById(int id);
+
+    /*
+     * @Query("SELECT p FROM Pizza p WHERE p.id = :id")
+     * Pizza findPizzaById(@Param("id") int id);
+     */
 }
