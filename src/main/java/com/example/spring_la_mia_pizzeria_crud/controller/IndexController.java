4d02c93f858feea0a0f81c91f22477db1fc03133
PaoloFabris98 <f98paolo@gmail.com>
2025-02-15 08:13:40 +0100

    feat:dont work

--- src/main/java/com/example/spring_la_mia_pizzeria_crud/controller/IndexController.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_crud/controller/IndexController.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/")
@@ -33,4 +35,11 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/pizza")
+    public String seePizza(@RequestParam(name = "query") String query, Model model) {
+        System.out.println("Query Parameter: " + query);
+        model.addAttribute("pizza", pizzaRepository.findByNome(query));
+        model.addAttribute("isValid", "true");
+        return "pizza";
+    }
 }
--- src/main/java/com/example/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
@@ -3,8 +3,6 @@ package com.example.spring_la_mia_pizzeria_crud.repository;
 import java.util.ArrayList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.spring_la_mia_pizzeria_crud.model.Pizza;
@@ -12,4 +10,6 @@ import com.example.spring_la_mia_pizzeria_crud.model.Pizza;
 @Repository
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
     ArrayList<Pizza> findById(int id);
+
+    ArrayList<Pizza> findByNome(String name);
 }
