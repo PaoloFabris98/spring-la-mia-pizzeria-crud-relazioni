89466cf03ec689b95c6b03f43a190bd75a89a8f3
PaoloFabris98 <f98paolo@gmail.com>
2025-02-25 19:51:36 +0100

    feat:service2

--- src/main/java/com/example/spring_la_mia_pizzeria_crud/controller/IndexController.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_crud/controller/IndexController.java
@@ -1,25 +1,13 @@
 package com.example.spring_la_mia_pizzeria_crud.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.example.spring_la_mia_pizzeria_crud.model.Pizza;
 import com.example.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequestMapping("/")
